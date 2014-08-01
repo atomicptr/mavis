3444d81d76b975fa9dcb965387785f85498c845a
Christopher Kaster <ikasoki@gmail.com>
2014-08-01 12:04:41 +0200

    remove example, fix linkstuff

--- premake4.lua
+++ premake4.lua
@@ -1,20 +1,17 @@
 solution "mavis"
-	configurations { "debug" }
-	
-	language "C++"
-	includedirs { "mavis/include" }
-	
-	buildoptions "-std=c++11"
+    configurations { "debug", "release" }
 
-	-- remove ldflags to fix broken dead code elimination stuff
-	premake.gcc.platforms.x32.ldflags = nil
-	premake.gcc.platforms.x64.ldflags = nil
-	
-	project "mavis"
-		kind "SharedLib"
-		files { "mavis/source/**.cpp" }
-		
-	project "mavis-test"
-		kind "ConsoleApp"
-		files { "test/**.cpp" }
-		links { "mavis" }
+    language "C++"
+    includedirs { "mavis/include" }
+
+    buildoptions { "-std=c++11" }
+
+    -- remove ldflags to fix broken dead code elimination stuff
+    premake.gcc.platforms.ldflags = nil
+
+    project "mavis"
+        kind "SharedLib"
+        files { "mavis/source/**.cpp" }
+
+        configuration { "debug" }
+            flags { "Symbols" }
